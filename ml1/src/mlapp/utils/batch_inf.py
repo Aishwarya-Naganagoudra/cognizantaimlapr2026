fbf0d369aade86ac856ac65481de7192cd767ed2
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-05-25 12:38:25 +0530

    updated

--- ml1/src/mlapp/utils/batch_inf.py
+++ ml1/src/mlapp/utils/batch_inf.py
@@ -8,8 +8,7 @@ def batch_inference(reviews_csv):
     reviews_df = pd.read_csv(reviews_csv)
     
     # Initialize the sentiment analysis pipeline
-    sentiment_pipeline = pipeline("sentiment-analysis", 
-                                  model="distilbert-base-uncased-finetuned-sst-2-english" )
+    sentiment_pipeline = pipeline("sentiment-analysis")
     
     # Perform batch inference on the reviews
     results = sentiment_pipeline(reviews_df['review_text'].tolist())
@@ -18,9 +17,12 @@ def batch_inference(reviews_csv):
     reviews_df['sentiment'] = [result['label'] for result in results]
     #confidence scores
     reviews_df["confidence_score"] = [round(r["score"], 4) for r in results]
+    reviews_df.to_csv("reviews_with_sentiment.csv", index=False)  # Save the results to a new CSV file
     return reviews_df
 
 if __name__ == "__main__":
     reviews_csv = Config.REVIEW_PATH  # Path to your reviews CSV file
     results_df = batch_inference(reviews_csv)
+    #read results from csv and print the first 5 rows
+    results_df = pd.read_csv("reviews_with_sentiment.csv")
     print(results_df.head())
